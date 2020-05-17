83fa012fb24aea3b8b2b9e5d747f5b22ea37d9e1
VirtualBrightPlayz <lego.trains.b@gmail.com>
2020-05-17 11:29:56 -0700

    files

---
+++ NoAmmoCap/NoAmmoCap/PluginEvents.cs
@@ -0,0 +1,127 @@
+﻿using EXILED;
+using EXILED.ApiObjects;
+using EXILED.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoAmmoCap
+{
+    public class PluginEvents
+    {
+        private PluginMain plugin;
+        private ItemType[] allowed = new ItemType[]
+        {
+            ItemType.GunCOM15,
+            ItemType.GunE11SR,
+            ItemType.GunLogicer,
+            ItemType.GunMP7,
+            ItemType.GunProject90,
+            ItemType.GunUSP,
+        };
+
+        public PluginEvents(PluginMain pluginMain)
+        {
+            this.plugin = pluginMain;
+        }
+
+        internal void Shoot(ref ShootEvent ev)
+        {
+            if (!PluginMain.ammo.ContainsKey(ev.Shooter))
+                PluginMain.ammo.Add(ev.Shooter, new Ammo(ev.Shooter));
+            ItemType id = ev.Shooter.inventory.curItem;
+            if (id.IsWeapon(false))
+            {
+                ev.Shooter.SetAmmo(AmmoType.Dropped5, 1);
+                ev.Shooter.SetAmmo(AmmoType.Dropped7, 1);
+                ev.Shooter.SetAmmo(AmmoType.Dropped9, 1);
+            }
+        }
+
+        internal void Spawn(PlayerSpawnEvent ev)
+        {
+            if (PluginMain.ammo.ContainsKey(ev.Player))
+                PluginMain.ammo.Remove(ev.Player);
+        }
+
+        internal void PlyCmd(ConsoleCommandEvent ev)
+        {
+            if (!PluginMain.ammo.ContainsKey(ev.Player))
+                PluginMain.ammo.Add(ev.Player, new Ammo(ev.Player));
+            if (ev.Command.ToLower() == "ammo")
+            {
+                ev.Player.SendConsoleMessage(PluginMain.ammo[ev.Player].ToString(), "cyan");
+                ev.ReturnMessage = "";
+            }
+        }
+
+        internal void Reset()
+        {
+            PluginMain.ammo.Clear();
+        }
+
+        public int MaxAmmo(ReferenceHub hub)
+        {
+            return hub.weaponManager.weapons.First(i => i.inventoryID == hub.GetCurrentItem().id).maxAmmo;
+        }
+
+        internal void ReloadItem(ref PlayerReloadEvent ev)
+        {
+            if (!PluginMain.ammo.ContainsKey(ev.Player))
+                PluginMain.ammo.Add(ev.Player, new Ammo(ev.Player));
+            var id = ev.Player.GetCurrentItem();
+            if (PluginMain.ammo[ev.Player].Get() > 0)
+            {
+                ev.Allow = true;
+                int dur = (int)Pickup.Inv.GetItemByID(ev.Player.inventory.curItem).durability;
+                PluginMain.ammo[ev.Player].Add(-dur);
+                if (PluginMain.ammo[ev.Player].Get() <= 0)
+                    PluginMain.ammo[ev.Player].Set(0);
+                int newammo = (int)id.durability + dur + PluginMain.ammo[ev.Player].Get();
+                if (newammo > MaxAmmo(ev.Player))
+                {
+                    PluginMain.ammo[ev.Player].Set(-(MaxAmmo(ev.Player) - newammo));
+                    newammo = MaxAmmo(ev.Player);
+                }
+                ev.Player.SetWeaponAmmo(id, newammo);
+                ev.Player.SetAmmo(AmmoType.Dropped5, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped7, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped9, 1);
+            }
+            else
+            {
+                ev.Allow = false;
+                ev.Player.SetAmmo(AmmoType.Dropped5, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped7, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped9, 1);
+            }
+        }
+
+        internal void ItemChange(ItemChangedEvent ev)
+        {
+            if (!PluginMain.ammo.ContainsKey(ev.Player))
+                PluginMain.ammo.Add(ev.Player, new Ammo(ev.Player));
+            if (ev.NewItem.id.IsWeapon(false))
+            {
+                ev.Player.SetAmmo(AmmoType.Dropped5, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped7, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped9, 1);
+            }
+        }
+
+        internal void PickupEvent(ref PickupItemEvent ev)
+        {
+            if (!PluginMain.ammo.ContainsKey(ev.Player))
+                PluginMain.ammo.Add(ev.Player, new Ammo(ev.Player));
+            if (ev.Item.ItemId.IsAmmo())
+            {
+                ev.Allow = false;
+                PluginMain.ammo[ev.Player].Add(ev.Item.info.itemId, (int)ev.Item.info.durability);
+                ev.Player.SetAmmo(AmmoType.Dropped5, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped7, 1);
+                ev.Player.SetAmmo(AmmoType.Dropped9, 1);
+                ev.Item.Delete();
+            }
+        }
+    }
+}
---
+++ NoAmmoCap/NoAmmoCap/PluginMain.cs
@@ -0,0 +1,119 @@
+﻿using EXILED;
+using EXILED.ApiObjects;
+using EXILED.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoAmmoCap
+{
+    public class PluginMain : Plugin
+    {
+        public override string getName => "NoAmmoCap";
+        public static Dictionary<ReferenceHub, Ammo> ammo = new Dictionary<ReferenceHub, Ammo>();
+        public PluginEvents PLEV;
+
+        public override void OnDisable()
+        {
+            Events.ShootEvent -= PLEV.Shoot;
+            Events.PickupItemEvent -= PLEV.PickupEvent;
+            Events.ItemChangedEvent -= PLEV.ItemChange;
+            Events.PlayerReloadEvent -= PLEV.ReloadItem;
+            Events.RoundRestartEvent -= PLEV.Reset;
+            Events.ConsoleCommandEvent -= PLEV.PlyCmd;
+            Events.PlayerSpawnEvent -= PLEV.Spawn;
+            PLEV = null;
+        }
+
+        public override void OnEnable()
+        {
+            PLEV = new PluginEvents(this);
+            Events.ShootEvent += PLEV.Shoot;
+            Events.PickupItemEvent += PLEV.PickupEvent;
+            Events.ItemChangedEvent += PLEV.ItemChange;
+            Events.PlayerReloadEvent += PLEV.ReloadItem;
+            Events.RoundRestartEvent += PLEV.Reset;
+            Events.ConsoleCommandEvent += PLEV.PlyCmd;
+            Events.PlayerSpawnEvent += PLEV.Spawn;
+        }
+
+        public override void OnReload()
+        {
+        }
+    }
+
+    public class Ammo
+    {
+        public int[] ammo;
+        private ReferenceHub ply;
+
+        public Ammo(ReferenceHub ply)
+        {
+            this.ply = ply;
+            ammo = new int[ply.ammoBox.types.Length];
+            for (int i = 0; i < ply.ammoBox.types.Length; i++)
+                ammo[i] = ply.ammoBox.GetAmmo(i);
+        }
+
+        public void Add(int amount)
+        {
+            ammo[GetItemAmmoType(ply.GetCurrentItem().id)] += amount;
+            ply.SetAmmo(AmmoType.Dropped5, 1);
+            ply.SetAmmo(AmmoType.Dropped7, 1);
+            ply.SetAmmo(AmmoType.Dropped9, 1);
+        }
+
+        public void Add(ItemType type, int amount)
+        {
+            for (int i = 0; i < ply.ammoBox.types.Length; i++)
+            {
+                if (ply.ammoBox.types[i].inventoryID == type)
+                {
+                    ammo[i] += amount;
+                    break;
+                }
+            }
+            ply.SetAmmo(AmmoType.Dropped5, 1);
+            ply.SetAmmo(AmmoType.Dropped7, 1);
+            ply.SetAmmo(AmmoType.Dropped9, 1);
+        }
+
+        public int GetItemAmmoType(ItemType gun)
+        {
+            for (int i = 0; i < ply.weaponManager.weapons.Length; i++)
+            {
+                if (ply.weaponManager.weapons[i].inventoryID == gun)
+                    return ply.weaponManager.weapons[i].ammoType;
+            }
+            return -1;
+        }
+
+        public void Set(int amount)
+        {
+            ammo[GetItemAmmoType(ply.GetCurrentItem().id)] = amount;
+            ply.SetAmmo(AmmoType.Dropped5, 1);
+            ply.SetAmmo(AmmoType.Dropped7, 1);
+            ply.SetAmmo(AmmoType.Dropped9, 1);
+        }
+
+        public int Get()
+        {
+            ply.SetAmmo(AmmoType.Dropped5, 1);
+            ply.SetAmmo(AmmoType.Dropped7, 1);
+            ply.SetAmmo(AmmoType.Dropped9, 1);
+            return ammo[GetItemAmmoType(ply.GetCurrentItem().id)];
+        }
+
+        public override string ToString()
+        {
+            string str = "Ammo:";
+            for (int i = 0; i < ply.ammoBox.types.Length; i++)
+            {
+                str += "\n" + ply.ammoBox.types[i].label + " - " + ammo[i];
+            }
+            return str;
+        }
+    }
+}
